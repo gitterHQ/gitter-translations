68ad4d431f5f3a50142371f8fcba38cbc87a6c99
Vlad <dev@vld.me>
2015-02-26 22:56:18 +0300

    Update ru.js

--- ru.js
+++ ru.js
@@ -1,19 +1,19 @@
 {
 	"%s ago": "%s назад",
 	"%d unread message": {
-		"one": "%d unread message",
-		"other": "%d unread messages"
+		"one": "%d непрочитанное сообщение",
+		"other": "%d непрачитанных сообщений"
 	},
 	"Unread messages in %%s, %%s and one other": {
 		"one": "Unread messages in %%s, %%s and one other",
 		"other": "Unread messages in %%s, %%s and %d others"
 	},
-	"Unread messages in %s": "Unread messages in %s",
-	"Unread messages in %s and %s": "Unread messages in %s and %s",
-	"Unread messages from %s": "Unread messages from %s",
-	"Unread messages from %s and %s": "Unread messages from %s and %s",
+	"Unread messages in %s": "Непрочитанное сообщение в %s",
+	"Unread messages in %s and %s": "Непрочитанное сообщение в %s и %s",
+	"Unread messages from %s": "Непрочитанное сообщение от %s",
+	"Unread messages from %s and %s": "Непрочитанное сообщение от %s и %s",
 	"Unread messages from %%s, %%s and one other": {
-		"one": "Unread messages from %%s, %%s and one other",
-		"other": "Unread messages from %%s, %%s and %d others"
+		"one": "Непрочитанные сообщения от %%s и %%s",
+		"other": "Непрочитанные сообщения от %%s, %%s, %d и других"
 	}
 }
