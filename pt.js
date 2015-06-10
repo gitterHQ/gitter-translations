2a5f64c202373293a6d8c75c7a6e2b5dfa2e92ac
Ian Ramos <ianramosc@gmail.com>
2015-06-10 07:51:09 -0300

    Fixed and translated some things in 'pt'

--- pt.js
+++ pt.js
@@ -5,15 +5,15 @@
 		"other": "%d mensagens não lidas"
 	},
 	"Unread messages in %%s, %%s and one other": {
-		"one": "Unread messages in %%s, %%s and one other",
-		"other": "Unread messages in %%s, %%s and %d others"
+		"one": "Mensagens não lidas em %%s, %%s e um outro",
+		"other": "Mensagens não lidas em %%s, %%s e %d outros"
 	},
-	"Unread messages in %s": "Unread messages in %s",
-	"Unread messages in %s and %s": "Unread messages in %s and %s",
-	"Unread messages from %s": "Unread messages from %s",
-	"Unread messages from %s and %s": "Unread messages from %s and %s",
+	"Unread messages in %s": "Mensagens não lidas em %s",
+	"Unread messages in %s and %s": "Mensagens não lidas em %s e %s",
+	"Unread messages from %s": "Mensagens não lidas de %s",
+	"Unread messages from %s and %s": "Mensagens não lidas de %s e %s",
 	"Unread messages from %%s, %%s and one other": {
-		"one": "Unread messages from %%s, %%s and one other",
-		"other": "Unread messages from %%s, %%s and %d others"
+		"one": "Mensagens não lidas de %%s, %%s e mais um",
+		"other": "Mensagens não lidas de %%s, %%s e %d outros"
 	}
 }
