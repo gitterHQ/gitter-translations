858ffb198957b645abae3e7cf1e98d2330730948
tuhaihe <1132321739qq@gmail.com>
2015-07-20 07:42:39 +0800

    Update zh.js

--- zh.js
+++ zh.js
@@ -5,15 +5,15 @@
 		"other": "%d条未读信息"
 	},
 	"Unread messages in %%s, %%s and one other": {
-		"one": "Unread messages in %%s, %%s and one other",
-		"other": "Unread messages in %%s, %%s and %d others"
+		"one": "在%%s, %%s和另一组中有未读消息",
+		"other": "在 %%s, %%s 和其他 %d 个组中有未读消息"
 	},
-	"Unread messages in %s": "Unread messages in %s",
-	"Unread messages in %s and %s": "Unread messages in %s and %s",
-	"Unread messages from %s": "Unread messages from %s",
-	"Unread messages from %s and %s": "Unread messages from %s and %s",
+	"Unread messages in %s": "在 %s 中有未读消息",
+	"Unread messages in %s and %s": "在 %s 和 %s 中有未读消息",
+	"Unread messages from %s": "来自 %s 的未读消息",
+	"Unread messages from %s and %s": "来自 %s 和 %s 的未读消息",
 	"Unread messages from %%s, %%s and one other": {
-		"one": "Unread messages from %%s, %%s and one other",
-		"other": "Unread messages from %%s, %%s and %d others"
+		"one": "来自 %%s, %%s 和另一组的未读消息",
+		"other": "来自 %%s, %%s 和其他 %d 个组的未读消息"
 	}
 }
