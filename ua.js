0cfd20f71e880ff35462974d9b79d51ce2d927e6
mrdimonzz@gmail.com <cgfyxgslth>
2015-08-14 14:56:07 +0300

    Added Ukrainian translation

---
+++ ua.js
@@ -0,0 +1,18 @@
+{
+	"%d unread message": {
+		"one": "%d непрочитане повідомлення",
+		"other": "%d непрочитаних пвідомлень"
+	},
+	"Unread messages in %%s, %%s and one other": {
+		"one": "Непрочитані повідомлення в %%s, %%s і ще одному",
+		"other": "Непрочитані повідомлення в %%s, %%s і в %d інших"
+	},
+	"Unread messages in %s": "Непрочитані повідомлення в %s",
+	"Unread messages in %s and %s": "Непрочитані повідомлення в %s і %s",
+	"Unread messages from %s": "Непрочитані повідомлення від %s",
+	"Unread messages from %s and %s": "Непрочитані повідомлення від %s і %s",
+	"Unread messages from %%s, %%s and one other": {
+		"one": "Непрочитані повідомлення від %%s, %%s і ще одного",
+		"other": "Непрочитані повідомлення від %%s, %%s і %d інших"
+	}
+}
