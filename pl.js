79e28874d9e23d82af124f6db521eb96e4bdade5
MarPiRK <MarPiRK@users.noreply.github.com>
2015-10-28 20:31:09 +0100

    Created Polish translation

--- pl.js
+++ pl.js
@@ -1,18 +1,18 @@
 {
 	"%d unread message": {
-		"one": "%d unread message",
-		"other": "%d unread messages"
+		"one": "%d nieprzeczytana wiadomość",
+		"other": "%d nieprzeczytanych wiadomości"
 	},
 	"Unread messages in %%s, %%s and one other": {
-		"one": "Unread messages in %%s, %%s and one other",
-		"other": "Unread messages in %%s, %%s and %d others"
+		"one": "Nieprzeczytane wiadomości w %%s, %%s i na jeszcze jednym kanale",
+		"other": "Nieprzeczytane wiadomości w %%s, %%s i na %d innych kanałach"
 	},
-	"Unread messages in %s": "Unread messages in %s",
-	"Unread messages in %s and %s": "Unread messages in %s and %s",
-	"Unread messages from %s": "Unread messages from %s",
-	"Unread messages from %s and %s": "Unread messages from %s and %s",
+	"Unread messages in %s": "Nieprzeczytane wiadmomości w %s",
+	"Unread messages in %s and %s": "Nieprzeczytane wiadomości w %s i %s",
+	"Unread messages from %s": "Nieprzeczytane wiadomości od %s",
+	"Unread messages from %s and %s": "Nieprzeczytane wiadomości od %s i %s",
 	"Unread messages from %%s, %%s and one other": {
-		"one": "Unread messages from %%s, %%s and one other",
-		"other": "Unread messages from %%s, %%s and %d others"
+		"one": "Nieprzeczytane wiadomości od %%s, %%s i jeszcze jednej osoby",
+		"other": "Nieprzeczytane wiadomości od %%s, %%s i %d innych osób"
 	}
 }
