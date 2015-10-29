fa996b3835ac194c198676ed184e8705853106c1
Jens W. Klein <jk@kleinundpartner.at>
2015-10-30 00:49:22 +0100

    polishing

--- de.js
+++ de.js
@@ -1,13 +1,13 @@
 {
 	"%d unread message": {
 		"one": "%d ungelesene Nachricht",
-		"other": "%d ungelesene Nachricht"
+		"other": "%d ungelesene Nachrichten"
 	},
 	"Unread messages in %%s, %%s and one other": {
 		"one": "Ungelesene Nachrichten in %%s, %%s und einem weiteren",
-		"other": "Ungelesene Nachrichten %%s, %%s und %d weiteren"
+		"other": "Ungelesene Nachrichten in %%s, %%s und %d weiteren"
 	},
-	"Unread messages in %s": "Eine ungelesene Nachricht in %s",
+	"Unread messages in %s": "Ungelesene Nachrichten in %s",
 	"Unread messages in %s and %s": "Ungelesene Nachrichten in %s und %s",
 	"Unread messages from %s": "Ungelesene Nachrichten von %s",
 	"Unread messages from %s and %s": "Ungelesene Nachrichten von %s und %s",
