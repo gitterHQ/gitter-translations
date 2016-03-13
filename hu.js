d1230963a0c787ce295f2c4c2d04bcd326fc19b8
Benjamin <benjamin@xdw.me>
2016-03-13 14:06:33 +0100

    Create hu.js
    Hungarian translation

---
+++ hu.js
@@ -0,0 +1,18 @@
+{
+	"%d unread message": {
+		"one": "%d olvasatlan üzenet",
+		"other": "%d olvasatlan üzenet"
+	},
+	"Unread messages in %%s, %%s and one other": {
+		"one": "Új üzenet itt: %%s, %%s és egy egyéb",
+		"other": "új üzenet itt: %%s, %%s és %d egyéb"
+	},
+	"Unread messages in %s": "Új üzenet itt: %s",
+	"Unread messages in %s and %s": "Új üzenet itt: %s és itt: %s",
+	"Unread messages from %s": "%s új üzenetet küldött",
+	"Unread messages from %s and %s": "%s és %s új üzenetet küldött",
+	"Unread messages from %%s, %%s and one other": {
+		"one": "%%s, %%s és egy egyéb olvasatlan üzenet",
+		"other": "%%s, %%s és %d egyéb olvasatlan üzenet"
+	}
+}
