ebb54a88e2db232ffb05170195d5761bf045f9b3
mataslt <matas-lt@hotmail.com>
2016-04-14 18:54:39 +0100

    Create lt.js
    I have created another translation for my native language, Lithuanian. If you have any questions or enquiries please don't hesitate to message me.
    
    Matas

---
+++ lt.js
@@ -0,0 +1,19 @@
+
+{
+	"%d unread message": {
+		"one": "%d žinutė neperskaityta",
+		"other": "%d žinutes neperskaityta"
+	},
+	"Unread messages in %%s, %%s and one other": {
+		"one": "žinutė neperskaityta %%s, %%s ir vienas kitas",
+		"other": "žinutė neperskaityta %%s, %%s ir %d kiti"
+	},
+	"Unread messages in %s": "žinutė neperskaityta %s",
+	"Unread messages in %s and %s": "žinutė neperskaityta %s ir %s",
+	"Unread messages from %s": "žinutes neperskaitytos is %s",
+	"Unread messages from %s and %s": "žinutes neperskaitytos is %s ir %s",
+	"Unread messages from %%s, %%s žinutes neperskaitytos is": {
+		"one": "žinutes neperskaitytos is %%s, %%s ir vienas kita",
+		"other": "žinutes neperskaitytos is %%s, %%s ir %d kita"
+	}
+}
