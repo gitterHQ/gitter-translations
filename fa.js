40130eb533a4f2f773bd940a679c113836c0e4bb
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2016-04-28 05:44:20 +0430

    Create fa.js

---
+++ fa.js
@@ -0,0 +1,18 @@
+{
+	"%d unread message": {
+		"one": "%d پیام جدید",
+		"other": "%d پیام جدید"
+	},
+	"Unread messages in %%s, %%s and one other": {
+		"one": "پیام جدید در %%s، %%s و یک جای دیگر",
+		"other": "پیام جدید در %%s، %%s و %d جای دیگر"
+	},
+	"Unread messages in %s": "پیام جدید در %s",
+	"Unread messages in %s and %s": "پیام جدید در %s و %s",
+	"Unread messages from %s": "پیام جدید از %s",
+	"Unread messages from %s and %s": "پیام جدید از %s و %s",
+	"Unread messages from %%s, %%s and one other": {
+		"one": "پیام جدید از %%s، %%s و یک جای دیگر",
+		"other": "پیام جدید از %%s، %%s و %d جای دیگر"
+	}
+}
