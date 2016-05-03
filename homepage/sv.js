5a569fdeae0e25216c5f8d31c7429193c0baf136
Matte A <matte_med_latte@hotmail.com>
2016-05-03 18:29:22 +0200

    WIP

--- homepage/sv.js
+++ homepage/sv.js
@@ -2,26 +2,27 @@
 	"Integrations": "Integrationer",
 	"Pricing": "Prisplaner",
 	"Apps": "Appar",
-	"Existing user login": "Inloggning existerande användare" ,
+	"Existing user login": "Inloggning för existerande användare" ,
 	"login": "inloggning",
-	"Chat, for GitHub.": "Chat, för GitHub.",
-	"Get unlimited public rooms and one-to-one chats, free forever. Private team plans start from $2 per user per month. No credit card required.": "få oändligt med öppna rum och en-och-en rum, gratis för alltid. Privata team prisplaner startar på $2 per användare per månad. Inget kreditkort behövs.",
+	"Chat, for GitHub.": "Chatt, för GitHub.",
+	"Get unlimited public rooms and one-to-one chats, free forever. Private team plans start from $2 per user per month. No credit card required.": "Få oändligt med öppna rum och privata en-och-en rum, gratis för alltid. Privata team prisplaner startar på $2 per användare per månad. Inget kreditkort behövs.",
 	"Sign up free with Github": "Registrera dig gratis med GitHub",
-	"Sign up with Github": "Registrera dig med github",
-	"By signing up you agree to our <a %s>Terms and Conditions</a>": "genom att registrera dig godkänner du våra <a %s>Villkor</a>",
-	"Markdown power": "Markdown power",
-	"Ever wanted full markdown support in chat? How about syntax highlighting?": "någonsin önskar full markdown i chattar? Vad sägs som syntax highlighting?",
-	"Yip, we got your back.": "Jup, Det har vi.",
+	"Sign up with Github": "Registrera dig med Github",
+	"By signing up you agree to our <a %s>Terms and Conditions</a>": "Genom att registrera dig godkänner du våra <a %s>Terms and Conditions</a>",
+	"Markdown power": "Markdown power", ***
+	
+	"Ever wanted full markdown support in chat? How about syntax highlighting?": "Någonsin önskat full markdown i chattar? Vad sägs som syntax markeringar?", *
+	"Yip, we got your back.": "Yepp, det har vi.", *
 	"Private conversations": "Privata konversationer",
-	"Public rooms are awesome (and free), but we get that some stuff is private. So we've got <a %s>plans for that</a>.": "Publika rum är super (och gratis), men vi fattar att vissa saker är privata. Så vi har <a %s>prisplaner för det/a>.",
+	"Public rooms are awesome (and free), but we get that some stuff is private. So we've got <a %s>plans for that</a>.": "Offentliga rum är super (och gratis), men vi fattar att vissa saker är privata. Så vi har <a %s>prisplaner för det/a>.",*
 	"Developer friendly": "Utvecklarvänligt",
-	"This is a picture of a cloud against a background of clouds. OMG! We <a %s>integrate</a> with a bunch of stuff in the cloud.": "detta är en bild av ett moln, mot en bakgrund av moln. OMG! Vi <a %s>Integrerar</a> med en del grejer i molnet.",
+	"This is a picture of a cloud against a background of clouds. OMG! We <a %s>integrate</a> with a bunch of stuff in the cloud.": "Detta är en bild på ett moln, mot en bakgrund av moln. OMG! Vi <a %s>Integrerar</a> med en del grejer i molnet.",*
 	"Some of our communities": "Några av våra grupper",
-	"Create your own": "Skapa din egen",
+	"Create your own": "Skapa din egna",
 	"Explore rooms": "Utforska rum",
 	"Explore": "Utforska",
 	"Integrated with your workflow": "Integrerad med ditt arbetsflöde",
-	"Gitter is built on top of GitHub and is tightly integrated with your organisations, repositories, issues and activity.": "Gitter är byggt på GitHub och är nära integrerat med dina organisationer, repositories, issues och aktivitet.",
+	"Gitter is built on top of GitHub and is tightly integrated with your organisations, repositories, issues and activity.": "Gitter är byggt på GitHub och är nära integrerat med dina organisationer, repositories, issues och aktivitet.",*
 	"We provide integrations with Trello, Jenkins, Travis CI, Heroku, Sentry, BitBucket, HuBoard, Logentries, Pagerduty &amp; Sprintly. We also accept custom webhooks, have an <a %s>open source repository</a> for integrations as well as a <a %s>flexible API</a>.": "Vi har integreringar för Trello, Jenkins, Travis CI, Heroku, Sentry, BitBucket, HuBoard, Logentries, Pagerduty &amp; Sprintly. Vi accepterar även egna webhooks, har en <a %s>open source repository</a> för integrationer samt en <a %s>flexibel API</a>.",
 	"Loved by our users": "Älskat av våra användare",
 	"Gitter is nothing without the people using it day in and day out.": "Gitter är ingenting utan våra användare som använder det dag ut och dag in.",
@@ -30,7 +31,7 @@
 	"Free": "Gratis",
 	"Forever": "För alltid",
 	"<b>Unlimited</b> public rooms": "<b>Oändligt</b> med publika rum",
-	"<b>1</b> private room": "<b>1</b> privat rum",
+	"<b>1</b> private room": "<b>1</b> privatrum",
 	"<b>2 weeks’</b> chat history": "<b>2 veckors</b> chat historik",
 	"<b>Get started</b> for free": "<b>Kom igång</b> Gratis",
 	"Individual": "Individuellt",
@@ -41,27 +42,35 @@
 	"Organisation pricing": "organisationsprisplaner",
 	"Bronze": "Brons",
 	"per user/month": "per användare per månad",
-	"<b>6 months’</b> chat history": "<b>6 månader’</b> chat historik",
+	"<b>6 months’</b> chat history": "<b>6 månader’</b> chatt historik",
 	"Silver": "Silver",
-	"Our free plan includes one private room per organisation you belong to on GitHub": "Var gartisplan erbjuder ett privat rum per organisation du tillhör på GitHub",
-	"Our Individual plan allows you to create private rooms using your personal account as the owner": "Our Individual plan allows you to create private rooms using your personal account as the owner",
+	"Our free plan includes one private room per organisation you belong to on GitHub": "Vår gartisplan erbjuder ett privatrum per organisation du tillhör på GitHub",
+
+	"Our Individual plan allows you to create private rooms using your personal account as the owner": "Vår Individuella prisplan tillåter dig att skapa privata rum genom att använda ditt personliga konto som ägare",
+
 	"Our organisation plans allow you to create private rooms using your GitHub organisation as the owner": "Our organisation plans allow you to create private rooms using your GitHub organisation as the owner",
-	"Prices may be subject to VAT. By signing up you agree to our <a %s>Terms and Conditions</a>.": "Prices may be subject to VAT. By signing up you agree to our <a %s>Terms and Conditions</a>.",
-	"Get Gitter now!": "Get Gitter now!",
-	"Gitter is available in all modern browsers as well as apps for desktops and mobile phones.": "Gitter is available in all modern browsers as well as apps for desktops and mobile phones.",
-	"We currently have apps for OSX, iOS, Android and Windows with a Linux app coming soon. We also have a sweet little <a %s>IRC bridge</a>": "We currently have apps for OSX, iOS, Android and Windows with a Linux app coming soon. We also have a sweet little <a %s>IRC bridge</a>",
+
+	"Prices may be subject to VAT. By signing up you agree to our <a %s>Terms and Conditions</a>.": "Prices may be subject to moms. Genom att registera dig godkänner du våra <a %s>Terms and Conditions</a>.",
+
+	"Get Gitter now!": "Skaffa Gitter nu!",
+
+	"Gitter is available in all modern browsers as well as apps for desktops and mobile phones.": "Gitter är tillgängligt i alla moderna webbläsare as well as apps för desktops och mobiltelefoner.",
+	
+	"We currently have apps for OSX, iOS, Android and Windows with a Linux app coming soon. We also have a sweet little <a %s>IRC bridge</a>": "Vi har appar för OSX, iOS, Android och Windows med en Linux app påväg. Vi har också en mysig liten <a %s>IRC brygga</a>",
 	"Mac": "Mac",
 	"Windows": "Windows",
-	"Coming soon": "Coming soon",
+	"Coming soon": "Kommer snart",
 	"iPhone": "iPhone",
 	"Android": "Android",
-	"By developers, for developers": "By developers, for developers",
-	"Gitter is built in London by a team of developers just like you. Hell even the product guy writes code, although we generally end up rewriting those bits.": "Gitter is built in London by a team of developers just like you. Hell even the product guy writes code, although we generally end up rewriting those bits.",
-	"If you're ever around Silicon Roundabout, give us a shout, we love meeting up with other developers.": "If you're ever around Silicon Roundabout, give us a shout, we love meeting up with other developers.",
+	"By developers, for developers": "Av utvecklare, för utvecklare",
+
+	"Gitter is built in London by a team of developers just like you. Hell even the product guy writes code, although we generally end up rewriting those bits.": "Gitter är skapat i London av ett lag med utvecklare precis som du. För tusan, till och med the product guy skriver kod, även om vi brukar skriva om alla de bitarna.",
+
+	"If you're ever around Silicon Roundabout, give us a shout, we love meeting up with other developers.": "Om du någonsin är runt Silicon Roundabout, kom förbi, vi älskar att träffa andra utvecklare.",
 	"Support": "Support",
 	"API": "API",
-	"Blog": "Blog",
+	"Blog": "Blogg",
 	"Twitter": "Twitter",
-	"Gitter &mdash; Where developers come to talk.": "Gitter &mdash; Where developers come to talk.",
-	"Translated By": "Översatt Av"
+	"Gitter &mdash; Where developers come to talk.": "Gitter &mdash; Vart utvecklare kommer för att prata.",
+	"Translated By": "Mattias A (Nubbie on GitHub)"
 }
