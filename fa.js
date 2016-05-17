e25af46d30e0abb179be79100d7f62898a5fed1e
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2016-05-17 15:44:34 +0430

    Update fa.js

--- fa.js
+++ fa.js
@@ -1,18 +1,18 @@
 {
 	"%d unread message": {
 		"one": "%d پیام جدید",
-		"other": "%d پیام جدید"
+		"other": "%d پیام دیگر"
 	},
 	"Unread messages in %%s, %%s and one other": {
 		"one": "پیام جدید در %%s، %%s و یک جای دیگر",
-		"other": "پیام جدید در %%s، %%s و %d جای دیگر"
+		"other": "پیام دیگر در %%s، %%s و %d جای دیگر"
 	},
-	"Unread messages in %s": "پیام جدید در %s",
-	"Unread messages in %s and %s": "پیام جدید در %s و %s",
-	"Unread messages from %s": "پیام جدید از %s",
-	"Unread messages from %s and %s": "پیام جدید از %s و %s",
+	"Unread messages in %s": "پیام خوانده‌نشده در %s",
+	"Unread messages in %s and %s": "پیام خوانده‌نشده در %s و %s",
+	"Unread messages from %s": "پیام خوانده‌نشده از %s",
+	"Unread messages from %s and %s": "پیام خوانده‌نشده از %s و %s",
 	"Unread messages from %%s, %%s and one other": {
-		"one": "پیام جدید از %%s، %%s و یک جای دیگر",
-		"other": "پیام جدید از %%s، %%s و %d جای دیگر"
+		"one": "پیام خوانده‌نشده از %%s، %%s و یک جای دیگر",
+		"other": "پیام خوانده‌نشده از %%s، %%s و %d جای دیگر"
 	}
 }
