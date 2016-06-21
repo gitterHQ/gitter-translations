3c31cef408acc03caae17480d111754a5fe25b86
Andrew Newdigate <andrew@troupe.co>
2016-06-21 19:18:06 +0100

    🚚 Refactor: moved zh-CN to messages

--- messages/lt.js
+++ messages/lt.js
@@ -1,4 +1,3 @@
-
 {
 	"%d unread message": {
 		"one": "%d žinutė neperskaityta",
@@ -15,5 +14,9 @@
 	"Unread messages from %%s, %%s žinutes neperskaitytos is": {
 		"one": "žinutes neperskaitytos is %%s, %%s ir vienas kita",
 		"other": "žinutes neperskaitytos is %%s, %%s ir %d kita"
+	},
+	"Unread messages from %%s, %%s and one other": {
+		"one": "Unread messages from %%s, %%s and one other",
+		"other": "Unread messages from %%s, %%s and %d others"
 	}
 }
