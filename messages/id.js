3efff6274499f3f4152471760d91b0813e8bc633
Diki Ananta <diki1aap@gmail.com>
2017-06-17 12:17:44 +0700

    Add Indonesian language on Messages

---
+++ messages/id.js
@@ -0,0 +1,18 @@
+{
+    "%d unread message": {
+        "one": "%d pesan yang belum dibaca",
+        "other": "%d pesan yang belum dibaca"
+    },
+    "Unread messages in %%s, %%s and one other": {
+        "one": "Pesan yang belum dibaca di %%s, %%s dan satu lainnya",
+        "other": "Pesan yang belum dibaca di %%s, %%s dan %d lainnya"
+    },
+    "Unread messages in %s": "Pesan yang belum dibaca di %s",
+    "Unread messages in %s and %s": "Pesan yang belum dibaca di %s dan %s",
+    "Unread messages from %s": "Pesan belum dibaca dari %s",
+    "Unread messages from %s and %s": "Pesan belum dibaca dari %s dan %s",
+    "Unread messages from %%s, %%s and one other": {
+        "one": "Pesan belum dibaca dari %%s, %%s dan satu lainnya",
+        "other": "Pesan belum dibaca dari %%s, %%s dan %d lainnya"
+    }
+}
