71fdd9c76f7a09daa23dc466a9e1e54875f8ae8d
Murat Bastas <muratbsts@gmail.com>
2017-08-11 14:51:36 +0300

    Create Turkish translation

---
+++ messages/tr.js
@@ -0,0 +1,18 @@
+{
+	"%d unread message": {
+		"one": "%d okunmamış mesaj",
+		"other": "%d okunmamış mesaj"
+	},
+	"Unread messages in %%s, %%s and one other": {
+		"one": "%%s, %%s ve diğer okunmamış mesaj",
+		"other": "%%s, %%s ve %d diğer okunmamış mesaj"
+	},
+	"Unread messages in %s": "%s okunmamış mesajlar",
+	"Unread messages in %s and %s": "%s ve %s okunmamış mesajlar",
+	"Unread messages from %s": "%s kişisinden okunmamış mesajlar",
+	"Unread messages from %s and %s": "%s ve %s kişilerinden okunmamış mesajlar",
+	"Unread messages from %%s, %%s and one other": {
+		"one": "%%s, %%s ve bir başkasından okunmamış mesajlar",
+		"other": "%%s, %%s ve %d başka kişiden mesajlar"
+	}
+}
